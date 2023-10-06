3332e08cc81f56397a6d7d32cc1f56eed2addc67
big_e_v <viet.ly@joinklover.com>
2023-10-06 10:43:18 -0700

    fix: upgrade to v3 blankline

--- init.lua
+++ init.lua
@@ -153,10 +153,15 @@ require('lazy').setup({
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
+    main = "ibl",
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
+      whitespace = {
+        remove_blankline_trail = false
+      }
     },
   },
 
